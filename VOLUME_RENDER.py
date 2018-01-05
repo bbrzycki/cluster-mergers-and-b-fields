4e7c5a81449e97ce3e6a635337c74082b00cac18
Bryan Brzycki <bbrzycki@college.harvard.edu>
2018-01-05 04:59:44 -0500

    Bug fix volume render 1to1_b0

--- VOLUME_RENDER.py
+++ VOLUME_RENDER.py
@@ -2,14 +2,18 @@ import yt
 import numpy as np
 from yt.visualization.volume_rendering.transfer_function_helper import TransferFunctionHelper
 from yt.visualization.volume_rendering.api import Scene, VolumeSource
+import glob
+
 
 full_path_header="/data/mimir/jzuhone/data/fid_mag/1to1_b0/fiducial_1to1_b0_mag_hdf5_plt_cnt_"
 
-data_fns = m.glob.glob(full_path_header + "0[0-9][0-9][05]")
+data_fns = glob.glob(full_path_header + "0[0-9][0-9][05]")
 # Sort them
 data_fns.sort()
 
-ts = m.yt.DatasetSeries(data_fns)
+ts = yt.DatasetSeries(data_fns)
 for ds in ts:
+    print("Creating scene",str(ds)[-4:])
     sc = yt.create_scene(ds)
     sc.save('RENDERED_IMAGES/%s.png' % str(ds), sigma_clip=8)
+    print("Finishing image",str(ds)[-4:])
