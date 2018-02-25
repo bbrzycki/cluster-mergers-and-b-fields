2d430d27074ce36eb5404cbd5ec5afbf18d73eea
Bryan Brzycki <bbrzycki@college.harvard.edu>
2018-02-25 17:33:11 -0500

    try

--- magnolia/__init__.py
+++ magnolia/__init__.py
@@ -5,6 +5,10 @@ magnetic fields to the physics within the cores of galaxy cluster mergers
 
 __version__ = "1.0"
 
-#import derived_field_definitions
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+
+import derived_field_definitions
 
 from dataset_read_write_functions import *
