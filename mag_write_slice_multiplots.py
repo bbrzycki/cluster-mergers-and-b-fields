aeaac647ba5a3131a17dd17775215adddf708f83
Bryan Brzycki <bbrzycki@berkeley.edu>
2018-06-13 11:17:42 -0700

    Fix bracket

--- mag_write_slice_multiplots.py
+++ mag_write_slice_multiplots.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
         multiplot_info = [['density',ds_header_plt_cnt,1.0e-30,1.0e-25,'arbre',plot_dir+'/'+sim_name+'_density_multiplot.pdf'],
                           ['magnetic_field_strength',ds_header_plt_cnt,1e-10,1e-5,'arbre',plot_dir+'/'+sim_name+'_magnetic_field_strength_multiplot.pdf'],
                           ['kT',ds_header_plt_cnt,0.5,20,'algae',plot_dir+'/'+sim_name+'_kT_multiplot.pdf'],
-                          [('deposit','all_cic'),ds_header_part,1e-31,1e-25,'cubehelix',plot_dir+'/'+sim_name+'_all_cic_multiplot.pdf']]]
+                          [('deposit','all_cic'),ds_header_part,1e-31,1e-25,'cubehelix',plot_dir+'/'+sim_name+'_all_cic_multiplot.pdf']]
 
         for (field,ds_header,zlim1,zlim2,cmap,output_fn) in multiplot_info:
             ds_paths = ds_header + all_epochs[i]
